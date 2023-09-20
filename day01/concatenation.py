46c88a9183fffecc780bbd048807ad4334ac9c1f
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-09-20 13:26:45 +0300

    concatenations

---
+++ day01/concatenation.py
@@ -0,0 +1,21 @@
+# we can use + sing or {} or format() method for concatenation
+name = "ismail"
+print("My name is " + name)
+
+# print("My age is " + 32) # Error
+
+age = 32
+print("My age is {}".format(age))
+
+print(f"My age is {age}")  # f stands for format function
+
+print("My name is {} and I am {}".format(name, age))
+
+# another concatenation
+
+print('Python', 3, 'is awesome:', True)
+# ","  sign add spaces right and left sides
+
+# context of object
+# reference(memory address) of object
+
