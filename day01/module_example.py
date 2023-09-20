5e62041e2d71f0fbcf3686382c5eba57f737359d
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-09-20 14:06:29 +0300

    module example and finished first day

---
+++ day01/module_example.py
@@ -0,0 +1,18 @@
+
+print('Java')
+
+
+variable = 'Python'
+
+def test():
+    pass
+
+
+class Animal: #when we need to custom object we create and use class
+    pass
+
+
+"""
+packages are like dependencies
+.........
+"""
