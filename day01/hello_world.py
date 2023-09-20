10e65fb274fa468721e2bcae637c61091dd2bf43
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-09-20 13:25:02 +0300

    first day has started with Hello World

---
+++ day01/hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+print('Hello again!')
+
+"""..."""
+
+# single line comment
+
+
+print('Cydeo\nSchool')
+
+print('I love "Cydeo"')  # we dont need to use escape sequences for using " while creating a string with ''
+print("I love \"Cydeo\"")
+print("I love 'Cydeo'") # we dont need to use escape sequences for using ' while creating a string with ""
+
+
+print(__name__)
